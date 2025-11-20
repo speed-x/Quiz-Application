45c3f26e2f4390201ba8c84c5e1ad5c26e80ec0d
Priyanshu <priyxsh005@gmail.com>
2025-11-20 14:30:54 +0530

    Add quiz application with Question and service classes
    Introduced a Question model and a QuestionService to manage quiz logic, including question presentation, user input, and score calculation. Updated main.java to run the quiz and display the score.

--- 1/main.java
+++ 1/main.java
@@ -1,4 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
 
-public class main {
-    
+        QuestionService questionService = new QuestionService();
+        questionService.playQuiz();
+        questionService.printScore();
+    }
 }
